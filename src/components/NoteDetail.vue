7a9d0ae18869894abd4dbed80b0d9b3d28089242
BlueYang <2931442305@qq.com>
2022-01-23 11:35:03 +0800

    vuex 数据管理

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="detail" id="note">
-    <note-sidebar @update:notes="val => notes = val"/>
+    <note-sidebar @update:notes="val => this.notes = val"/>
     <ol class="note-detail">
       <li class="note-empty" v-show="!curNote.id">
         请选择笔记
       </li>
-      <li  class="note-detail-ct" v-show="curNote.id">
-        <ol  class="note-detail-ct">
+      <li class="note-detail-ct" v-show="curNote.id">
+        <ol class="note-detail-ct">
           <li class="note-bar">
             <span>创建日期：{{ curNote.createdAtFriendly }}</span>
             <span>更新日期：{{ curNote.updatedAtFriendly }}</span>
             <span>{{ statusText }}</span>
-            <span class="iconfont icon-delete" @click="deleteNote"></span>
+            <span class="iconfont icon-delete" @click="onDeleteNote"></span>
             <span class="iconfont icon-fullscreen" @click="isShowPreview = !isShowPreview"></span>
           </li>
           <li class="note-title">
-            <input type="text" v-model:value="curNote.title" @input="updateNote" @keydown="statusText='正在输入...'"
+            <input type="text" v-model:value="curNote.title" @input="onUpdateNote" @keydown="statusText='正在输入...'"
                    placeholder="请输入标题">
           </li>
           <li class="editor">
-            <textarea v-show="!isShowPreview" v-model:value="curNote.content" @keydown="statusText='正在输入...'" @input="updateNote"
+            <textarea v-show="!isShowPreview" v-model:value="curNote.content" @keydown="statusText='正在输入...'"
+                      @input="onUpdateNote"
                       placeholder="输入内容，支持markdown语法"></textarea>
             <div class="preview markdown-body" v-html="previewContent" v-show="isShowPreview"></div>
           </li>
@@ -30,63 +31,69 @@
 </template>
 
 <script>
-import Auth from '../api/auth'
 import NoteSidebar from './NoteSidebar'
-import Bus from '../helper/bus'
 import _ from 'lodash'
-import Notes from '../api/note'
 import MarkdownIt from 'markdown-it'
+import {mapGetters, mapMutations, mapActions} from 'vuex'
+import Auth from "../api/auth";
 
 let md = new MarkdownIt()
 export default {
-  name: 'NoteDetail',
   components: {
     NoteSidebar
   },
   data() {
     return {
-      curNote: {},
-      notes: [],
       statusText: '笔记未改动',
-      isShowPreview:false
+      isShowPreview: false
     }
   },
   methods: {
-    updateNote: _.debounce(function () {
-      Notes.updateNote({noteId: this.curNote.id}, {title: this.curNote.title, content: this.curNote.content})
+    ...mapMutations([
+      'setCurNote'
+    ]),
+
+    ...mapActions([
+      'updateNote',
+      'deleteNote',
+    ]),
+    onUpdateNote: _.debounce(function () {
+      this.updateNote({noteId: this.curNote.id, title: this.curNote.title, content: this.curNote.content})
         .then(() => {
           this.statusText = '已保存'
         }).catch(res => {
         this.$message.info(this.statusText = res.msg || '保存出错')
       })
     }, 300),
-    deleteNote() {
-      Notes.deleteNote({noteId: this.curNote.id})
-        .then(data => {
-          this.$message.success(data.msg || '删除成功')
-          this.notes.splice(this.notes.indexOf(this.curNote),1)
-          this.$router.replace({path:'/note'})
+    onDeleteNote() {
+      this.deleteNote({noteId: this.curNote.id})
+        .then(() => {
+          this.$message.success('删除成功')
+          this.$router.replace({path: '/note'})
         })
     }
   },
-  computed:{
-    previewContent(){
+  computed: {
+    ...mapGetters([
+      'notes',
+      'curNote',
+    ]),
+
+    previewContent() {
       return md.render(this.curNote.content || '')
     }
   },
   created() {
     Auth.getInfo()
-      .then(res => {
-        if (!res.isLogin) {
+      .then(({isLogin}) => {
+        if (!isLogin) {
+          this.$message.error('登录后才能操作')
           this.$router.push({path: '/login'})
         }
       })
-    Bus.$once('update:notes', val => {
-      this.curNote = val.find(note => note.id == this.$route.query.noteId) || {}
-    })
   },
   beforeRouteUpdate(to, from, next) {
-    this.curNote = this.notes.find(note => note.id == to.query.noteId) || {}
+    this.setCurNote({curNoteId: to.query.noteId})
     next()
   }
 }
@@ -115,9 +122,11 @@ export default {
     text-align: center;
     margin-top: 100px;
   }
-  .note-detail-ct{
+
+  .note-detail-ct {
     height: 100%;
   }
+
   .note-bar {
     padding: 4px 20px;
     border-bottom: 1px solid #eee;
@@ -181,6 +190,5 @@ export default {
   code {
     color: #f66;
   }
-
 }
 </style>
